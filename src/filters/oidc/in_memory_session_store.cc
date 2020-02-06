b9acab7d77e82f9db5666ebfbbf2fb65d404f326
Andrew Chang <anchang@pivotal.io>
2020-02-06 12:12:17 -0800

    Make session store operations thread safe
    - Using recursive_mutex and unique_lock because some functions call
    other functions
    - Introduce synchronized alias for mutex'ing
    
    Signed-off-by: Ryan Richard <rrichard@pivotal.io>

--- src/filters/oidc/in_memory_session_store.cc
+++ src/filters/oidc/in_memory_session_store.cc
@@ -1,3 +1,4 @@
+#include <spdlog/spdlog.h>
 #include "in_memory_session_store.h"
 
 namespace authservice {
@@ -12,25 +13,30 @@ InMemorySessionStore::InMemorySessionStore(std::shared_ptr<common::utilities::Ti
     max_session_idle_timeout_in_seconds_(max_session_idle_timeout_in_seconds) {}
 
 void InMemorySessionStore::Set(absl::string_view session_id, TokenResponse &token_response) {
-  //TODO: should this be synchronized somehow? can haz multithreads?
-  Remove(session_id);
-  map.emplace(session_id.data(),
-              std::make_shared<SessionData>(token_response, time_service_->GetCurrentTimeInSecondsSinceEpoch()));
+  synchronized(mutex_) {
+    Remove(session_id);
+    map.emplace(session_id.data(),
+                std::make_shared<SessionData>(token_response, time_service_->GetCurrentTimeInSecondsSinceEpoch()));
+  }
 }
 
-absl::optional<TokenResponse> InMemorySessionStore::Get(absl::string_view session_id) {
-  auto search = map.find(session_id.data());
-  if (search != map.end()) {
-    auto value = search->second;
-    value->SetTimeMostRecentlyAccessed(time_service_->GetCurrentTimeInSecondsSinceEpoch());
-    return absl::optional<TokenResponse>(value->GetTokenResponse());
-  } else {
-    return absl::nullopt;
+absl::optional <TokenResponse> InMemorySessionStore::Get(absl::string_view session_id) {
+  synchronized(mutex_) {
+    auto search = map.find(session_id.data());
+    if (search != map.end()) {
+      auto value = search->second;
+      value->SetTimeMostRecentlyAccessed(time_service_->GetCurrentTimeInSecondsSinceEpoch());
+      return absl::optional<TokenResponse>(value->GetTokenResponse());
+    } else {
+      return absl::nullopt;
+    }
   }
 }
 
 void InMemorySessionStore::Remove(absl::string_view session_id) {
-  map.erase(session_id.data());
+  synchronized(mutex_) {
+    map.erase(session_id.data());
+  }
 }
 
 void InMemorySessionStore::RemoveAllExpired() {
@@ -42,16 +48,18 @@ void InMemorySessionStore::RemoveAllExpired() {
   bool check_time_added = max_absolute_session_timeout_in_seconds_ > 0;
   bool check_time_idle = max_session_idle_timeout_in_seconds_ > 0;
 
-  auto it = map.begin();
-  while (it != map.end()) {
-    auto value = it->second;
-    bool expired_based_on_time_added = value->GetTimeAdded() < earliest_time_added_to_keep;
-    bool expired_based_on_idle_time = value->GetTimeMostRecentlyAccessed() < earliest_time_idle_to_keep;
+  synchronized(mutex_) {
+    auto it = map.begin();
+    while (it != map.end()) {
+      auto value = it->second;
+      bool expired_based_on_time_added = value->GetTimeAdded() < earliest_time_added_to_keep;
+      bool expired_based_on_idle_time = value->GetTimeMostRecentlyAccessed() < earliest_time_idle_to_keep;
 
-    if ((check_time_added && expired_based_on_time_added) || (check_time_idle && expired_based_on_idle_time)) {
-      it = map.erase(it);
-    } else {
-      it++;
+      if ((check_time_added && expired_based_on_time_added) || (check_time_idle && expired_based_on_idle_time)) {
+        it = map.erase(it);
+      } else {
+        it++;
+      }
     }
   }
 }
@@ -59,20 +67,21 @@ void InMemorySessionStore::RemoveAllExpired() {
 SessionData::SessionData(TokenResponse &token_response, uint32_t time_added)
     : token_response_(token_response), time_added_(time_added), time_accessed_(time_added) {}
 
-TokenResponse &SessionData::GetTokenResponse() {
-  return token_response_;
+std::string SessionData::to_string() {
+  return "expiry:" + std::to_string(token_response_.GetAccessTokenExpiry().value());
 }
 
-uint32_t SessionData::GetTimeAdded() {
-  return time_added_;
-}
 
-uint32_t SessionData::GetTimeMostRecentlyAccessed() {
-  return time_accessed_;
-}
-
-void SessionData::SetTimeMostRecentlyAccessed(uint32_t time_accessed) {
-  time_accessed_ = time_accessed;
+std::string InMemorySessionStore::to_string() {
+  std::string output;
+  std::string result;
+  std::string convert;
+  for (const auto & it : map) {
+    convert = it.second->to_string();
+    output += (it.first) + ":" + (convert) + ", ";
+  }
+  result = output.substr(0, output.size() - 2);
+  return result;
 }
 
 }  // namespace oidc
